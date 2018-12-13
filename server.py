1dc8613d2e68d134293c7983e6385e49d242ea24
Georg Donner <georgdonner98@gmail.com>
2018-12-13 14:14:50 +0100

    Add view and logic to fork a playlist.

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 import os, urllib
 from dotenv import load_dotenv
-from flask import Flask, request, redirect, session
+from flask import Flask, request, redirect, render_template, session
 import requests
 import pymongo
 
@@ -18,7 +18,52 @@ def auth_header(access_token):
 
 @app.route('/')
 def index():
-  return 'Hello World'
+  return render_template('index.html')
+
+def create_playlist(name):
+  r = requests.post(
+    f'https://api.spotify.com/v1/users/{session["spotify_id"]}/playlists',
+    headers=auth_header(session['access_token']),
+    json={'name': name, 'public': False},
+  )
+  return r.json()
+
+def get_track_uris(data):
+  return [item['track']['uri'] for item in data['items']]
+
+def add_tracks(playlist_id, track_uris):
+  requests.post(
+    f'https://api.spotify.com/v1/playlists/{playlist_id}/tracks',
+    headers=auth_header(session['access_token']),
+    json={'uris': track_uris},
+  )
+
+def fork_playlist(playlist_uri):
+  playlist_id = playlist_uri.split(':').pop()
+  r = requests.get(
+    f'https://api.spotify.com/v1/playlists/{playlist_id}',
+    headers=auth_header(session['access_token']),
+  )
+  data = r.json()
+  created = create_playlist(data['name'])
+  # Switch to tracks as data, because the subsequent /tracks calls will return exactly that
+  data = data['tracks']
+  add_tracks(created['id'], get_track_uris(data))
+  while data['next']:
+    print('next page')
+    r = requests.get(
+      f'https://api.spotify.com/v1/playlists/{playlist_id}/tracks',
+      headers=auth_header(session['access_token']),
+    )
+    data = r.json()
+    add_tracks(created['id'], get_track_uris(data))
+  return created
+
+@app.route('/fork', methods=['POST'])
+def fork():
+  playlist_uri = request.form['playlist']
+  fork_playlist(playlist_uri)
+  return 'Forked!'
 
 scopes = ['playlist-modify-public', 'playlist-modify-private']
 auth_query_parameters = {
