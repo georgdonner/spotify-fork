6690fb9cf29d8247abf64e5aae3d878274e731f4
Georg Donner <georgdonner98@gmail.com>
2018-12-13 20:43:08 +0100

    Extract Spotify related requests and methods to own module.

--- server.py
+++ server.py
@@ -4,25 +4,12 @@ from flask import Flask, request, redirect, render_template, session
 import requests
 import pymongo
 
-CLIENT_ID = os.environ.get('SPOTIFY_CLIENT_ID')
-CLIENT_SECRET = os.environ.get('SPOTIFY_CLIENT_SECRET')
-REDIRECT_URI = os.environ.get('SPOTIFY_REDIRECT_URI')
+from spotify import Spotify
 
 load_dotenv()
 db = pymongo.MongoClient(os.environ.get('MONGODB_URI'))['spotify-fork']
 app = Flask(__name__)
-app.secret_key = CLIENT_SECRET
-
-def auth_header(access_token):
-  return { 'Authorization': f'Bearer {access_token}' }
-
-def update_token(refresh_token):
-  client = f'{CLIENT_ID}:{CLIENT_SECRET}'
-  base64encoded = base64.b64encode(client.encode())
-  headers = {'Authorization': f'Basic {base64encoded.decode()}'}
-  payload = {'grant_type': 'refresh_token', 'refresh_token': refresh_token}
-  r = requests.post('https://accounts.spotify.com/api/token', headers=headers, data=payload)
-  return r.json()
+app.secret_key = os.environ.get('SPOTIFY_CLIENT_SECRET')
 
 @app.route('/')
 def index():
@@ -31,19 +18,11 @@ def index():
     return render_template('index.html')
   if 'spotify_id' in session:
     user = db['users'].find_one({'spotify_id': session['spotify_id']})
-    token_info = update_token(user['refresh_token'])
+    token_info = Spotify.update_token(user['refresh_token'])
     session['access_token'] = token_info['access_token']
     session['expires'] = datetime.datetime.now() + datetime.timedelta(0, token_info['expires_in'])
     return render_template('index.html')
   return redirect('/login')
-
-def create_playlist(name):
-  r = requests.post(
-    f'https://api.spotify.com/v1/users/{session["spotify_id"]}/playlists',
-    headers=auth_header(session['access_token']),
-    json={'name': name, 'public': False},
-  )
-  return r.json()
 
 def add_playlist(user, playlist_id, playlist_id_original):
   playlist = {
@@ -59,57 +38,32 @@ def add_playlist(user, playlist_id, playlist_id_original):
 def get_track_uris(data):
   return [item['track']['uri'] for item in data['items']]
 
-def add_tracks(playlist_id, track_uris):
-  requests.post(
-    f'https://api.spotify.com/v1/playlists/{playlist_id}/tracks',
-    headers=auth_header(session['access_token']),
-    json={'uris': track_uris},
-  )
-
-def fork_playlist(playlist_uri):
-  playlist_id = playlist_uri.split(':').pop()
-  r = requests.get(
-    f'https://api.spotify.com/v1/playlists/{playlist_id}',
-    headers=auth_header(session['access_token']),
-  )
-  data = r.json()
-  created = create_playlist(data['name'])
-  add_playlist(session['spotify_id'], created['id'], playlist_id)
+def fork_playlist(spotify, playlist_uri):
+  playlist_id = Spotify.get_playlist_id(playlist_uri)
+  data = spotify.get_playlist(playlist_id)
+  created = spotify.create_playlist(data['name'])
   # Switch to tracks as data, because the subsequent /tracks calls will return exactly that
   data = data['tracks']
-  add_tracks(created['id'], get_track_uris(data))
+  spotify.add_tracks(created['id'], get_track_uris(data))
   while data['next']:
-    r = requests.get(
-      f'https://api.spotify.com/v1/playlists/{playlist_id}/tracks',
-      headers=auth_header(session['access_token']),
-    )
-    data = r.json()
-    add_tracks(created['id'], get_track_uris(data))
+    data = spotify.request(data['next'])
+    spotify.add_tracks(created['id'], get_track_uris(data))
+  add_playlist(session['spotify_id'], created['id'], playlist_id)
   return created
 
 @app.route('/fork', methods=['POST'])
 def fork():
   playlist_uri = request.form['playlist']
-  fork_playlist(playlist_uri)
+  spotify = Spotify(session['spotify_id'], session['access_token'])
+  fork_playlist(spotify, playlist_uri)
   return 'Forked!'
-
-scopes = ['playlist-modify-public', 'playlist-modify-private']
-auth_query_parameters = {
-  'response_type': 'code',
-  'scope': ' '.join(scopes),
-  # 'state': STATE,
-  'client_id': CLIENT_ID,
-  'redirect_uri': REDIRECT_URI,
-}
 
 @app.route('/login')
 def login():
-  params = '&'.join([f'{key}={urllib.parse.quote(val)}' for key, val in auth_query_parameters.items()])
-  return redirect(f'https://accounts.spotify.com/authorize?{params}')
+  return redirect(Spotify.get_redirect_url())
 
-def create_user(access_token, refresh_token):
-  r = requests.get('https://api.spotify.com/v1/me', headers=auth_header(access_token))
-  profile = r.json()
+def create_user(spotify, refresh_token):
+  profile = spotify.get_profile()
   user = {
     'spotify_id': profile['id'],
     'refresh_token': refresh_token,
@@ -122,17 +76,10 @@ def create_user(access_token, refresh_token):
 @app.route('/callback')
 def callback():
   code = request.args['code']
-  code_payload = {
-    'grant_type': 'authorization_code',
-    'code': str(code),
-    'redirect_uri': REDIRECT_URI,
-    'client_id': CLIENT_ID,
-    'client_secret': CLIENT_SECRET,
-  }
-  r = requests.post('https://accounts.spotify.com/api/token', data=code_payload)
-  post_data = r.json()
-  user = create_user(post_data['access_token'], post_data['refresh_token'])
-  session['access_token'] = post_data['access_token']
-  session['expires'] = datetime.datetime.now() + datetime.timedelta(0, post_data['expires_in'])
+  token_info = Spotify.exchange_code(code)
+  spotify = Spotify(None, token_info['access_token'])
+  user = create_user(spotify, token_info['refresh_token'])
+  session['access_token'] = token_info['access_token']
+  session['expires'] = datetime.datetime.now() + datetime.timedelta(0, token_info['expires_in'])
   session['spotify_id'] = user['spotify_id']
   return redirect('/')
---
+++ spotify.py
@@ -0,0 +1,80 @@
+import base64, os, urllib
+import requests
+
+CLIENT_ID = os.environ.get('SPOTIFY_CLIENT_ID')
+CLIENT_SECRET = os.environ.get('SPOTIFY_CLIENT_SECRET')
+REDIRECT_URI = os.environ.get('SPOTIFY_REDIRECT_URI')
+
+SCOPES = ['playlist-modify-public', 'playlist-modify-private']
+
+class Spotify:
+  def __init__(self, user_id, access_token):
+    self.user_id = user_id
+    self.auth = {'Authorization': f'Bearer {access_token}'}
+
+  def request(self, url, method='GET'):
+    r = requests.request(method, url, headers=self.auth)
+    return r.json()
+
+  def get_profile(self):
+    r = requests.get('https://api.spotify.com/v1/me', headers=self.auth)
+    return r.json()
+
+  def get_playlist(self, playlist_id):
+    r = requests.get(
+      f'https://api.spotify.com/v1/playlists/{playlist_id}',
+      headers=self.auth,
+    )
+    return r.json()
+
+  def create_playlist(self, name):
+    r = requests.post(
+      f'https://api.spotify.com/v1/users/{self.user_id}/playlists',
+      headers=self.auth,
+      json={'name': name, 'public': False},
+    )
+    return r.json()
+  
+  def add_tracks(self, playlist_id, track_uris):
+    requests.post(
+      f'https://api.spotify.com/v1/playlists/{playlist_id}/tracks',
+      headers=self.auth,
+      json={'uris': track_uris},
+    )
+
+  @staticmethod
+  def get_playlist_id(playlist_uri):
+    return playlist_uri.split(':').pop()
+
+  @staticmethod
+  def get_redirect_url():
+    auth_query_parameters = {
+      'response_type': 'code',
+      'scope': ' '.join(SCOPES),
+      # 'state': STATE,
+      'client_id': CLIENT_ID,
+      'redirect_uri': REDIRECT_URI,
+    }
+    params = '&'.join([f'{key}={urllib.parse.quote(val)}' for key, val in auth_query_parameters.items()])
+    return f'https://accounts.spotify.com/authorize?{params}'
+
+  @staticmethod
+  def exchange_code(code):
+    code_payload = {
+      'grant_type': 'authorization_code',
+      'code': str(code),
+      'redirect_uri': REDIRECT_URI,
+      'client_id': CLIENT_ID,
+      'client_secret': CLIENT_SECRET,
+    }
+    r = requests.post('https://accounts.spotify.com/api/token', data=code_payload)
+    return r.json()
+
+  @staticmethod
+  def update_token(refresh_token):
+    client = f'{CLIENT_ID}:{CLIENT_SECRET}'
+    base64encoded = base64.b64encode(client.encode())
+    headers = {'Authorization': f'Basic {base64encoded.decode()}'}
+    payload = {'grant_type': 'refresh_token', 'refresh_token': refresh_token}
+    r = requests.post('https://accounts.spotify.com/api/token', headers=headers, data=payload)
+    return r.json()
